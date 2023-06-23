4b6b7fb01d67ec3113ab6c404d8858d6effd81c8
Toll <paulipanther.hoesch@gmail.com>
2023-06-23 12:06:23 +0200

    yoooo

--- polybar/bluetooth.sh
+++ polybar/bluetooth.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#66ffffff}"
+	echo -e "%{F#66ffffff}%{T2}"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
-    echo ""
+    echo "%{T4}"
   fi
-  echo "%{F#2193ff}"
+  echo "%{F#2193ff}%{T2}"
 fi
 
